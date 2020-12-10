7863b1c78eee26c416ef083f002289d1d4633308
vaishnavirk0411 <kulkarnivaishnavi0411@gmail.com>
2020-12-10 13:35:38 +0530

    PASSWORD

--- form.php
+++ form.php
@@ -8,8 +8,8 @@
 
 <?php
 // define variables and set to empty values
-$nameErr = $emailErr  = $websiteErr = "";
-$name = $email  = $website = "";
+$nameErr = $emailErr  = $websiteErr =  $passwordErr= $cpasswordErr="";
+$name = $email  = $website = $password= $cpassword= "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
@@ -41,7 +41,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $websiteErr = "Invalid URL";
     }
   }
-
+  if (empty($_POST["password"])) {
+    $passwordErr = "Password is required";}
+    if (empty($_POST["cpassword"])) {
+    $cpasswordErr = "Confirm password is required";}
+//Validates password & confirm passwords.
+    if(!empty($_POST["password"]) && ($_POST["password"] == $_POST["cpassword"]))
+     {
+        $password = test_input($_POST["password"]);
+        $cpassword = test_input($_POST["cpassword"]);
+        if (strlen($_POST["password"]) <= '8') {
+            $passwordErr = "Your Password Must Contain At Least 8 Characters!";
+        }
+        elseif(!preg_match("#[0-9]+#",$password)) {
+            $passwordErr = "Your Password Must Contain At Least 1 Number!";
+        }
+        elseif(!preg_match("#[A-Z]+#",$password)) {
+            $passwordErr = "Your Password Must Contain At Least 1 Capital Letter!";
+        }
+        elseif(!preg_match("#[a-z]+#",$password)) {
+            $passwordErr = "Your Password Must Contain At Least 1 Lowercase Letter!";
+        }
+      }
+     elseif(!empty($_POST["password"])) {
+    $cpasswordErr = "Please enter same Password!";
+       } else {
+     $passwordErr = "Please enter password   ";
+      }
 }
 
 function test_input($data) {
@@ -56,16 +82,21 @@ function test_input($data) {
   margin-left: 630px; font-family: 'Philosopher',serif;">PHP FORM</h2>
 <p><span class="error"></span></p>
 <form class="form" method="post" style="margin-top: 10px;" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  <div class="container" style="padding: 10%;">&nbsp&nbsp&nbsp&nbsp&nbspName:&nbsp&nbsp&nbsp&nbsp&nbsp <input type="text" name="name" value="<?php echo $name;?>">
+  <div class="container" style="padding: 10%;">Name: <input type="text" name="name" value="<?php echo $name;?>">
   <span class="error"><br>*<?php echo $nameErr;?></span>
   <br><br>
-  &nbsp&nbsp&nbsp&nbsp&nbspE-mail:&nbsp&nbsp&nbsp&nbsp&nbsp <input type="text" name="email" value="<?php echo $email;?>">
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
   <span class="error"><br>* <?php echo $emailErr;?></span>
   <br><br>
-  &nbsp&nbsp&nbsp&nbspWebsite:&nbsp&nbsp <input type="text" name="website" value="<?php echo $website;?>">
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
   <span class="error"><br>* <?php echo $websiteErr;?></span>
   <br><br>
-  
+  Password: <input type="password" name="password" value="<?php echo $password;?>">
+  <span class="error"><br>* <?php echo $passwordErr;?></span>
+  <br><br>
+  Confirm Password: <input type="password" name="cpassword" value="<?php echo $cpassword;?>">
+  <span class="error"><br>* <?php echo $cpasswordErr;?></span>
+  <br><br>
 
   <br><br><div style="text-align: center; ">
     <button class="button" id="sub" type="submit" name="submit" value="Submit" style="background-color: #4CAF50;
